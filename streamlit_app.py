9c93d2818bf46f37f142b95c76124f97ae6de6c8
AndersonPala <anderson.palacios@bluetab.net>
2024-11-20 01:55:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+
 # Escribir directamente en la aplicación
 st.title(':cup_with_straw: Customize Your Smoothie! :cup_with_straw:')
 st.write(
